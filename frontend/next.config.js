95daf7f7169d3b0aaeb45a38d94544cdcf9440f3
shezafatima <sohailejaz058@gmail.com>
2026-01-24 20:38:21 +0500

    updted

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
+    NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || ""
   },
-}
+  
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
