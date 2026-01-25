6badaad8a0d2c11630e22a740f5c7d6b510fbb53
shezafatima <sohailejaz058@gmail.com>
2026-01-25 18:07:33 +0500

    next config update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,12 +4,7 @@ const nextConfig = {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
     NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || ""
   },
-  // Disable telemetry to avoid build issues
-  telemetry: false,
-  // Images config for when needed
-  images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'], // Add any image domains you use
-  },
+
 };
 
 module.exports = nextConfig;
